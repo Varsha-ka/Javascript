6c449bbdab706e9798cd3145bb0b52805cc75653
Varsha <varshakarajagi05@gmail.com>
2025-04-23 20:37:42 +0530

    Add files via upload

---
+++ JavaScript.js
@@ -0,0 +1,23 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Objects</title>
+</head>
+<body>
+    <script>
+        let product={
+         compnay:`Manngooo`,
+         price:861,
+         item_name:'t-shirt',
+        };
+
+        console.log(product);
+        console.log(product.compnay);
+        console.log(product.price);
+        console.log(product.item_name);
+    </script>
+</body>
+</html>
